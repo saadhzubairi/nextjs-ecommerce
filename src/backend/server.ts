d61f3282c8af943ecb5a416ec18fcfdd0361e196
Saad Zubairi <s.zubari_18656@khi.iba.edu.pk>
2024-04-05 16:50:12 +0500

    added mongoDb config and enabled payload CMS

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,6 +1,6 @@
 import express from "express"
 import { getPayloadClient } from "./get-payload"
-import { nextApp, nextHandler } from "@/next-utils"
+import { nextApp, nextHandler } from "../next-utils"
 
 const app = express()
 
@@ -12,10 +12,10 @@ const start = async () => {
         initOptions: {
             express: app,
             onInit: async (cms) => {
-                /* this is where the admin dahsboard will live. and we're just logging it*/
-                cms.logger.info(`Admin URL ${cms.getAdminURL}`)
-            }
-        }
+                /* where the admin dashboard shall live (we're just logging the url) */
+                cms.logger.info(`[>] Admin URL: ${cms.getAdminURL()}`)
+            },
+        },
     })
 
 
@@ -23,11 +23,11 @@ const start = async () => {
     app.use((req, res) => nextHandler(req, res))
 
     nextApp.prepare().then(() => {
-        payload.logger.info("[+] NEXT.JS STARTED.")
+        payload.logger.info("[+] Next.js started.")
     })
 
     app.listen(PORT, async () => {
-        payload.logger.info(`[>] NEXT.JS APP URL ${process.env.NEXT_PUBLIC_SERVER_URL}`)
+        payload.logger.info(`[>] Next.js App URL ${process.env.NEXT_PUBLIC_SERVER_URL}`)
     })
 
 }
